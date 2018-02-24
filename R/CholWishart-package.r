216b193818cf44b8fe752389a803e0ca5a86c831
gzt <gzthompson@gmail.com>
2018-02-24 00:59:03 -0600

    add useful things to package

---
+++ R/CholWishart-package.r
@@ -0,0 +1,7 @@
+#' CholWishart.
+#'
+#' A package for fast computation various functions related to the Wishart distribution, such as sampling from the Cholesky factor of the Wishart, sampling from the inverse Wishart, sampling from the Cholesky factor of the inverse Wishart, computing densities for the Wishart and inverse Wishart, and computing a few auxiliart functions such as the multivariate gamma and digamma functions. Many of these functions are written in C to maximize efficiency.
+#'
+#' @name CholWishart
+#' @docType package
+NULL
