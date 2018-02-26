3e2792c3a45ffbbd508421847f8216a27fe27411
Geoffrey Thompson <gzthompson@gmail.com>
2018-02-26 14:08:05 -0600

    include additional tests for density functions

--- tests/testthat/testinput.R
+++ tests/testthat/testinput.R
@@ -30,9 +30,23 @@ test_that("trying wrong type of input", {
   expect_error(dWishart("A", df = 4, Sigma = diag(5)))
   expect_error(dWishart(.4, df = 4, Sigma = diag(5)))
 
-  expect_error(dInvWishart(1, df = 10, Sigma = matrix(c(1,0,0,0,1,0,0,0,1,1,1,1),nrow=3)))
-  expect_error(dWishart(1, df = 10, Sigma = matrix(c(1,0,0,0,1,0,0,0,1,1,1,1),nrow=3)))
+  expect_error(dInvWishart(matrix(1,nrow=3, ncol=4), df = 10, 
+                           Sigma = matrix(c(1,0,0,0,1,0,0,0,1,1,1,1),nrow=3)))
+  expect_error(dWishart(matrix(1,nrow=3, ncol=4), df = 10, 
+                           Sigma = matrix(c(1,0,0,0,1,0,0,0,1,1,1,1),nrow=3)))
 
+  expect_error(dInvWishart(matrix(1,nrow=3, ncol=3), df = 10, 
+                           Sigma = matrix(c(1,0,0,0,1,0,0,0,1,1,1,1),nrow=3)))
+  expect_error(dWishart(matrix(1,nrow=3, ncol=3), df = 10, 
+                        Sigma = matrix(c(1,0,0,0,1,0,0,0,1,1,1,1),nrow=3)))
+  
+  sigma = diag(3)
+  sigma[3,1] <- 1
+  expect_error(dInvWishart(diag(3), df = 10, Sigma = sigma))
+  expect_error(dWishart(diag(3), df = 10, Sigma = sigma))
+  
+  expect_error(dInvWishart(sigma, df = 10, Sigma = diag(3)))
+  expect_error(dWishart(sigma, df = 10, Sigma = diag(3)))
 
   expect_error(rCholWishart(1, 10, matrix(c(3, 1, 1, 1, 1, 3), nrow = 2)))
   expect_error(rInvCholWishart(1, 10, matrix(c(3, 1, 1, 1, 1, 3), nrow = 2)))
--- tests/testthat/testoutput.R
+++ tests/testthat/testoutput.R
@@ -13,7 +13,7 @@ test_that("Testing helper functions:", {
   expect_equal((p * (p - 1) / 4 * log(pi) + lgamma(5 - 0) + lgamma(5 - .5)), as.numeric(lmvgamma(5, 2)))
   expect_equal((3 * (3 - 1) / 4 * log(pi) + lgamma(5 - 0) + lgamma(5 - .5) + lgamma(5 - 1)),
                as.numeric(lmvgamma(5, 3)))
-  expect_equal(digamma(1:100),mvdigamma(1:100,1))
+  expect_equal(digamma(1:100),c(mvdigamma(1:100,1)))
 })
 
 test_that("Equivalent outputs for different options:", {
@@ -36,6 +36,7 @@ test_that("Equivalent outputs for different options:", {
   B <- rWishart(1,10,.2*diag(5))[,,1]
   expect_equal(A %*% B, diag(5))
 
+  # this really shouldn't work in general, it only works on diag()
   expect_equal(dWishart(diag(5), 10, 5*diag(5)), dInvWishart(diag(5), 10, .2*diag(5)))
 
 })
